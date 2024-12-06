50f9b4b4a0310776ff16ffc4a7d4b518ba0df10a
caprie <caprie@live.fr>
2024-12-06 12:25:28 +0100

    MAJ commits login

---
+++ FrontEnd/script/login.js
@@ -0,0 +1,7 @@
+
+let login = document.querySelector("#email");
+let password = document.querySelector("#password");
+let submit = document.querySelector("input[type = submit]");
+console.log(submit)
+
+
